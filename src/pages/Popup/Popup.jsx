3a76830a402943fd1383f14c9772724868685b3c
osdnk <micosa97@gmail.com>
2023-05-05 08:54:38 -0700

    works

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../assets/img/logo.svg';
 import './Popup.css';
 import Capsule, { Modal, Environment } from '@usecapsule/web-sdk';
 
-const capsule = new Capsule(Environment.SANDBOX)
+const capsule = new Capsule(Environment.BETA)
 
 const Popup = () => {
   console.log("x")
+  const [visible, setVisible] = useState(false)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Modal capsule={capsule} isOpen={true} onClose={() => {}} />
+        <Modal capsule={capsule} isOpen={visible} onClose={() => { setVisible(false) }} />
         <p>
           Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
         </p>
+        <button onClick={() => { setVisible(true) }}>
+          OPEN
+        </button>
+        <button onClick={async () => {
+          const wallet = Object.keys(capsule.getWallets())[0]
+          // console.log(x)
+          const tx = await capsule.sendTransaction(wallet, "6YCFF0h26ACCUgiUQsmnLJ38ySyuDelRAWDOotonr5GF6NSlEACAgICA", "1")
+          console.log(tx)
+        }}>
+          SEND
+        </button>
         <a
           className="App-link"
           href="https://reactjs.org"
